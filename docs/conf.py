592669472940858cee3ae0b0115fb2f00cb5dccd
Yusen Peng <yusenrocks@gmail.com>
2024-07-17 14:17:33 -0400

    wow!!

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,15 @@
-
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../digitrecognition'))
+
+
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
